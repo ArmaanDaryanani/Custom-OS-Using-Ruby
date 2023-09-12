f882688ae540a22d6e84ccdb00e34137d6c8e30f
Armaan Daryanani <armaandaryanani@Malaysia.attlocal.net>
2023-09-11 23:28:55 -0500

    Initial commit

---
+++ TinyToken.rb
@@ -0,0 +1,56 @@
+#
+#  Class Token - Encapsulates the tokens in TINY
+#
+#   @type - the type of token (Category)
+#   @text - the text the token represents (Lexeme)
+#
+class Token
+  attr_accessor :type
+  attr_accessor :text
+
+  # This is the only part of this class that you need to
+  # modify.
+  EOF = "eof"
+  LPAREN = "("
+  RPAREN = ")"
+  ADDOP  = "+"
+  WS = "whitespace"
+  UNKWN = "unknown"
+  SUBOP = "-"
+  MULOP = "*"
+  DIVOP = "/"
+  PRINT = "print"
+  ASSIGN = "="
+  ID = "ID"
+  INT = "INT"
+
+  IF = "if"
+  THEN = "then"
+  WHILE = "while"
+  LT = "<"
+  GT = ">"
+  AND = "&"
+
+  # add the rest of the tokens needed based on the grammar
+  # specified in the Scanner class "TinyScanner.rb"
+
+  #constructor
+  def initialize(type,text)
+    @type = type
+    @text = text
+  end
+
+  def get_type
+    return @type
+  end
+
+  def get_text
+    return @text
+  end
+
+  # to string method
+  def to_s
+    return "#{@type} #{@text}"
+  end
+end# frozen_string_literal: true
+
